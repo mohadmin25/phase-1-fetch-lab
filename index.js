831f8b47db7ea1960279707764148882c4a8564c
mohadmin25 <abuhamar008@gmail.com>
2022-08-26 18:08:39 +0300

    done

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  return fetch('https://anapioficeandfire.com/api/books') //returns the fetch()
+  .then(resp => resp.json()) //The returned response should be converted to JSON.
+  .then(json => renderBooks(json)) //passing in the JSON-ified data as the argument.
 
+}
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
@@ -11,7 +13,6 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
-
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
